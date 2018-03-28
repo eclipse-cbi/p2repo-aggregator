8a51b34b097f591ac38f7b40fc3b2e331da62ab5
David Williams <david_williams@acm.org>
2018-03-28 15:16:46 -0400

    [releng] adding some local developer files
    Change-Id: I909c8ae36543bdaefc4ee48e67575e6b8e98e0af
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.cbi.p2repo.releng.parent/localScripts/runLocalBuild.sh
+++ org.eclipse.cbi.p2repo.releng.parent/localScripts/runLocalBuild.sh
@@ -3,14 +3,14 @@
 # Utility script to start build in this directory
 # Copyright 2018 
 
-# The gitCBIREPOdir variable MUST be set to the diectory that contains (i.e. is the parent of) the local clone of org.eclipse.cbi.p2repo.aggregator.
+# The gitCBIREPOdir variable MUST be set to the directory that contains (i.e. is the parent of) the local clone of org.eclipse.cbi.p2repo.aggregator.
 
-# This runlocalBuild.sh file is typcially copied up to that same directory, so its initial contents look as follows:
+# This runlocalBuild.sh file is typically copied up to that same directory, so its initial contents look as follows:
 #  runLocalBuild.sh
 #  org.eclipse.cbi.p2repo.aggregator
 
 # once ran, the runBuild.sh file creates a "cleanable" directory under gitCBIREPOdir also. This cleanable directory
-# in turn contains tmp and localMvnRepo which, as the name inplies, can be removed for a "clean" run. 
+# in turn contains tmp and localMvnRepo which, as the name implies, can be removed for a "clean" run. 
 
 gitCBIREPOdir=~/gitCBIPhoton
 
@@ -18,7 +18,13 @@ export WORKSPACE="${gitCBIREPOdir}"
 
 export build_home=${WORKSPACE:-"${gitCBIREPOdir}"}
 
-execDir="${build_home}/org.eclipse.cbi.p2repo.aggregator/org.eclipse.cbi.p2repo.releng.parent/buildScripts/"
+execDir="${build_home}/org.eclipse.cbi.p2repo.aggregator/org.eclipse.cbi.p2repo.releng.parent/buildScripts"
+
+if [[ ! -d "${execDir}" ]]
+then
+  printf "[ERROR] %s\n\t%s\n" "The required directory did not exist. Did you correctly specify 'gitCBIREPOdir'?" "gitCBIREPOdir: ${gitCBIREPOdir}"
+  exit 1
+fi
 
 execCMD="${execDir}/runBuild.sh"
 
